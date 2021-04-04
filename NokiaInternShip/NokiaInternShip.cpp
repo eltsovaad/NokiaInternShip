562c42eeda03b171b7e6c9c6b18eda98820fa49f
Anastasia Eltsova <eltsova_ad@hotmail.com>
2021-04-04 18:37:18 +0300

    Добавьте файлы проекта.

---
+++ NokiaInternShip/NokiaInternShip.cpp
@@ -0,0 +1,12 @@
+﻿// NokiaInternShip.cpp: определяет точку входа для приложения.
+//
+
+#include "NokiaInternShip.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
